4b00e6a0ad442b9a3b2e15fe4ebef45e75d69e84
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-08-16 16:08:33 +0530

    disabled css

--- src/Components/Pagination.tsx
+++ src/Components/Pagination.tsx
@@ -9,14 +9,14 @@ interface Props{
 const Pagination :React.FC<Props> = ({totalpages ,setPage,currentpage}) => {
   return (
     <div className="w-full flex justify-center items-center p-4 gap-4">
-      <button disabled={currentpage ===1} onClick={()=>setPage(currentpage-1)}>Prev</button>
+      <button disabled={currentpage ===1} onClick={()=>setPage(currentpage-1)} className="disabled:cursor-not-allowed">Prev</button>
       {setPages(totalpages).map((page)=>
           <div key={page} className={"p-4 border-2 border-red-500 cursor-pointer " + `${currentpage==page ? 'bg-red-700':''}`} onClick={()=> setPage(page)}>
              {page}
           </div>
       )}
       <button disabled={currentpage===totalpages} onClick={()=> {
-        setPage(currentpage+1)}}>Next</button>
+        setPage(currentpage+1)}} className="disabled:cursor-not-allowed">Next</button>
     </div>
   )
 }
